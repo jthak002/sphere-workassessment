c966e138a73977271c7710e2db6bd95900bcf618
Jeet Thakkar <jthak002@ucr.edu>
2018-02-24 23:48:37 -0800

    Completed Assignment

--- server.go
+++ server.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"database/sql"
 	"fmt"
-	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -20,17 +20,51 @@ type Customer struct {
 	address string
 }
 
-//TableBody stores the HTML formatted table rows with the corresposding data
-//for each customer record from the SQL database
-type TableBody struct {
-	TableFields []byte
+//PageBody stres the entirety of the page in a struct. The fixed parts are
+//indexPreTable and indexPostTable. tableFields stores the HTML formatted table content
+type PageBody struct {
+	indexPreTable  []byte
+	indexPostTable []byte
 }
+
+var pageBody PageBody
 
 //unexported global db variable to store the database open variable
 var db *sql.DB
 
+//pageRenderInit reads the pre - and post-table parts of the HTML document
+//since this doesn't change, the operation can be computed only once.
+func pageRenderInit() error {
+	indexPreTableLocal, err := ioutil.ReadFile("index-pre")
+	if err != nil {
+		log.Fatal("index.pre render failed")
+	}
+	indexPostTableLocal, err := ioutil.ReadFile("index-post")
+	if err != nil {
+		log.Fatal("index.post render failed")
+	}
+	pageBody = PageBody{indexPreTable: indexPreTableLocal,
+		indexPostTable: indexPostTableLocal}
+	return err
+}
+
+//createHTMLTable converts a given Customer slice into a byte stream
+//od HTML formatted Table Data
+func createHTMLTable(customers *[]Customer) []byte {
+	var buffer bytes.Buffer
+	for _, value := range *customers {
+		buffer.WriteString("<tr> ")
+		buffer.WriteString("<td>" + value.id + "</td> ")
+		buffer.WriteString("<td>" + value.name + "</td> ")
+		buffer.WriteString("<td>" + value.age + "</td> ")
+		buffer.WriteString("<td>" + value.address + "</td> ")
+		buffer.WriteString("</tr>\n")
+	}
+	return buffer.Bytes()
+}
+
 //ParseRecord function uses the Customer data structure and returns a string containing
-//the customer data fprmatted into a table component
+//the customer data formatted into a table component
 func (customer *Customer) ParseRecord() string {
 	tableData := "<tr><td>" + string(customer.id) + "</td><td>" + customer.name + "</td><td>" + string(customer.age) + "</td><td>" + customer.address + "</td></tr>"
 	return tableData
@@ -45,89 +79,6 @@ func createRecord(name string, age string, id string, address string) Customer {
 		address: address,
 	}
 	return customer
-}
-
-//createHTMLTable converts a given Customer slice into a byte stream
-//TRIED CONVERTING IT INTO string but the HTMLEscaper functions that are a part of
-//convert all the tags into escape sequences. Byte Streams get past those filters
-func createHTMLTable(customers *[]Customer) []byte {
-	var buffer bytes.Buffer
-	for _, value := range *customers {
-		buffer.WriteString("<tr> ")
-		buffer.WriteString("<td>" + value.id + "</td> ")
-		buffer.WriteString("<td>" + value.name + "</td> ")
-		buffer.WriteString("<td>" + value.age + "</td> ")
-		buffer.WriteString("<td>" + value.address + "</td> ")
-		buffer.WriteString("</tr>\n")
-	}
-	return buffer.Bytes()
-}
-
-//addRecord function accepts input from the form and then adds the input to
-//the MySQL Database. The page refreshes the page to display the new values of the table.
-func addRecord(writer http.ResponseWriter, request *http.Request) {
-	log.Println("RENDERING addRecord.html")
-	request.ParseForm()
-	formMap := request.Form
-	var cname, caddress, cid, cage string
-	for key, value := range formMap {
-		if key == "frm_name" {
-			cname = strings.Join(value, "")
-		} else if key == "frm_age" {
-			cage = strings.Join(value, "")
-		} else if key == "frm_id" {
-			cid = strings.Join(value, "")
-		} else if key == "frm_address" {
-			caddress = strings.Join(value, "")
-		}
-	}
-	customer := createRecord(cname, cage, cid, caddress)
-	log.Println("Record Created Successfully!")
-	log.Println(customer)
-	dbAdd(customer)
-	log.Println("Customer Added Successfully!")
-	customers := dbSpan()
-	log.Println(customers)
-	byteObject := TableBody{TableFields: createHTMLTable(&customers)}
-	t, _ := template.ParseFiles("index")
-	t.Execute(writer, byteObject)
-
-}
-
-//renderIndex function renders the index.html page on request from the clients
-func renderIndex(writer http.ResponseWriter, request *http.Request) {
-	log.Println("RENDERING index.html")
-	customers := dbSpan()
-	log.Println(customers)
-	byteObject := TableBody{TableFields: createHTMLTable(&customers)}
-	t, _ := template.ParseFiles("index")
-	t.Execute(writer, byteObject)
-}
-
-//dbAdd function receives an object of type customer and adds it to the
-//MySQL Database
-func dbAdd(customer Customer) {
-	err := db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-	var buffer bytes.Buffer
-	buffer.WriteString("INSERT INTO `CustomerData` (CustomerId, CustomerName, CustomerAge, CustomerAddress) VALUES (")
-	customerStringSlice := []string{customer.id, customer.name, customer.age, customer.address}
-	buffer.WriteString("'" + customerStringSlice[0] + "', '")
-	buffer.WriteString(customerStringSlice[1] + "', '")
-	buffer.WriteString(customerStringSlice[2] + "', '")
-	buffer.WriteString(customerStringSlice[3] + "');")
-	log.Println(buffer.String())
-	stmt, err := db.Prepare(buffer.String())
-	if err != nil {
-		log.Println(err)
-	}
-	res, err := stmt.Exec()
-	if err != nil {
-		log.Println(err)
-		log.Println(res)
-	}
 }
 
 //dbSpan function sends a query to MySQL Database and then collects the response
@@ -164,16 +115,94 @@ func dbSpan() []Customer {
 	return customers
 }
 
+//renderIndex function renders the index.html page on request from the clients
+func renderIndex(writer http.ResponseWriter, request *http.Request) {
+	log.Println("Server: render index.html")
+	customers := dbSpan()
+	tableFields := createHTMLTable(&customers)
+	preAndTable := append(pageBody.indexPreTable, tableFields...)
+	finalObject := append(preAndTable, pageBody.indexPostTable...)
+	fmt.Fprintf(writer, string(finalObject))
+
+}
+
+//dbAdd function receives an object of type customer and adds it to the
+//MySQL Database
+func dbAdd(customer Customer) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var buffer bytes.Buffer
+	//CONSOLIDATING THE ENTIRE SQL COMMAND INTO A SINGLE STATEMENT USING A bytes.Buffer
+	buffer.WriteString("INSERT INTO `CustomerData` (CustomerId, CustomerName, CustomerAge, CustomerAddress) VALUES (")
+	//POPULATING THE ACTUAL VALUES FROM THE SQL DATABASE INTO THE buffer
+	customerStringSlice := []string{customer.id, customer.name, customer.age, customer.address}
+	buffer.WriteString("'" + customerStringSlice[0] + "', '")
+	buffer.WriteString(customerStringSlice[1] + "', '")
+	buffer.WriteString(customerStringSlice[2] + "', '")
+	buffer.WriteString(customerStringSlice[3] + "');")
+	//USING Prepare() TO CREATE THE SQL COMMAND INTO a Statement OBJECT
+	stmt, err := db.Prepare(buffer.String())
+	if err != nil {
+		log.Println(err)
+	}
+	res, err := stmt.Exec()
+	if err != nil {
+		log.Println(err)
+		log.Println(res)
+	}
+}
+
+//addRecord function accepts input from the form and then adds the input to
+//the MySQL Database. The page refreshes the page to display the new values of the table.
+func addRecord(writer http.ResponseWriter, request *http.Request) {
+	log.Println("Server: render /addRecord")
+	request.ParseForm()
+	formMap := request.Form
+	var cname, caddress, cid, cage string
+	for key, value := range formMap {
+		if key == "frm_name" {
+			cname = strings.Join(value, "")
+		} else if key == "frm_age" {
+			cage = strings.Join(value, "")
+		} else if key == "frm_id" {
+			cid = strings.Join(value, "")
+		} else if key == "frm_address" {
+			caddress = strings.Join(value, "")
+		}
+	}
+	customer := createRecord(cname, cage, cid, caddress)
+	log.Println("MySQL.sphere:Record Created Successfully!")
+	dbAdd(customer)
+	log.Println("MySQL.sphereCustomer Added Successfully!")
+	customers := dbSpan()
+	tableFields := createHTMLTable(&customers)
+	html1 := append(pageBody.indexPreTable, tableFields...)
+	finalObject := append(html1, pageBody.indexPostTable...)
+	fmt.Fprintf(writer, string(finalObject))
+}
+
 func main() {
-	var err error
+	var err error          //func main shared error variable
+	err = pageRenderInit() //Read the Pre-Table and Post-Table part of the HTML Page
+	if err != nil {
+		log.Println("Page Render Failed!")
+	} else {
+		log.Println("Page Render Successful!")
+	}
+	//Initialize the connection to the SQL Database
 	db, err = sql.Open("mysql", "sphere:SphereWorkAssessment234()@tcp(127.0.0.1:3306)/sphere")
+	//SQL Error Connection Troubleshooting
 	if err != nil {
 		log.Printf("MySQL.Sphere not responding%e\n", err)
 	}
+	//SQL Connection Life Check - Performed before each SQL Transaction
 	err = db.Ping()
 	if err != nil {
 		log.Println("MySQL.Sphere Offline")
 	}
+	//If SQL Connection established and Live display SUCCESS
 	if err != nil {
 		log.Println("MySQL.Sphere DB Init Failed")
 	} else {
@@ -182,5 +211,6 @@ func main() {
 	defer db.Close()
 	http.HandleFunc("/addRecord", addRecord)
 	http.HandleFunc("/index.html", renderIndex)
+	http.HandleFunc("/", renderIndex)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
